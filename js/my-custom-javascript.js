9b5ec68fa721f0a5674d5372553927039b301e20
Matt Lopez <matthew.lopez4011@gmail.com>
2019-08-23 14:55:28 -0500

    j

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,11 +1,39 @@
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// $(document).ready(function() {
+//     var coolerHeader = "Howdy, Ceres! Let's talk jQuery!";
+//    $('#header').html(coolerHeader);
+//     $('.lorem').css('color', 'blue').css( 'font-size', '2em').css('border', '2px solid blue');
+//     $('h1, p').css('text-decoration', 'underline');
+// });
+//
+//
+// $(document).ready(function() {
+// var alertText = $('#text').html();
+//     console.log(alertText);
+//
+//     alert(alertText );
+//
+//     $('.codeup, #codeup').css('border', '1px solid red')
+//
+// });
 
 $(document).ready(function() {
-    var coolerHeader = "Howdy, Ceres! Let's talk jQuery!";
-   $('#header').html(coolerHeader);
-    $('.lorem').css('color', 'blue').css( 'font-size', '2em').css('border', '2px solid blue');
-    $('h1, p').css('text-decoration', 'underline');
+    $('li').css('font-size', '20px')
+
+    $('h1, p, li').css('color', 'yellow')
+
+    var alertH1 = $('h1').html();
+    console.log(alertH1);
+    alert(alertH1);
+
 });
+
+
+
+
+// Craft selectors that highlight all the h1, p, and li elements.
+
 
