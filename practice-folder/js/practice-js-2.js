0945926ee4d89927e46b3ad40ae3a4cda252993d
Matt Lopez <matthew.lopez4011@gmail.com>
2019-10-29 22:39:48 -0500

    practice selecting and styling  DOM elements with javascript

--- practice-folder/js/practice-js-2.js
+++ practice-folder/js/practice-js-2.js
@@ -9,11 +9,24 @@ for (var i = 0; i < words.length; i++){
     var word = words[i];
     var firstLetter = word[0].toUpperCase(); // Uppercase first letter
     var restOfWord = word.slice(1);
-    console.log("Word: " + i + " " + firstLetter + restOfWord);
+    // console.log("Word: " + i + " " + firstLetter + restOfWord);
     uppercaseWords.push(firstLetter + restOfWord);
 
 }
 return uppercaseWords;
 }
 
-console.log(upperCaseFirstLetter());
+// console.log(upperCaseFirstLetter());
+
+
+// ============================================================================================
+
+var h1Element = document.querySelector(".one");
+h1Element.insertAdjacentHTML('afterend', "Yaaaaayyyy!");
+h1Element.style.color = 'red';
+
+var h2Element = document.querySelector(".two");
+// h2Element.css("background", "black").css("color", "blue");
+h2Element.style.backgroundColor = "black";
+h2Element.style.color = "blue";
+h2Element.style.display = "inline-block";
